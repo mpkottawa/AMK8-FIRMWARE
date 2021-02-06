077941b5ad346ee4668d5801a8b1c7efdcabe9b3
mpkottawa <mpkottawa@gmail.com>
2021-02-06 17:11:57 -0500

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -748,7 +748,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 450, 450, 25, 120 }
+#define DEFAULT_MAX_FEEDRATE          { 450, 450, 50, 120 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
