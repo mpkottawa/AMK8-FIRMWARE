a35f752f955a936416e64de612544acb335debf8
mpkottawa <mpkottawa@gmail.com>
2021-02-06 19:56:43 -0500

    UPDATE

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "2021-02-06 5:09pm"
+#define STRING_DISTRIBUTION_DATE "2021-02-06 7:52pm"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
@@ -54,7 +54,7 @@
  * has a distinct Github fork— the Source Code URL should just be the main
  * Marlin repository.
  */
-//#define SOURCE_CODE_URL "https://github.com/MarlinFirmware/Marlin"
+#define SOURCE_CODE_URL "https://github.com/mpkottawa/AMK8-FIRMWARE"
 
 /**
  * Default generic printer UUID.
