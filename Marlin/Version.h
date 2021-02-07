9a5c99475b32ba53fc20dadadfb1c8a93b88426b
mpkottawa <mpkottawa@gmail.com>
2021-02-06 20:04:31 -0500

    UPDATE

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.0.420.06"
+#define SHORT_BUILD_VERSION "2.0.420.07"
 
 /**
  * Verbose version identifier which should contain a reference to the location
