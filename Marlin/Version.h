fc7c53aa68badfb4d9010175b0381f1a40e5e658
mpkottawa <mpkottawa@gmail.com>
2023-04-30 15:36:25 -0400

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.0.420.03.13.22 LA fan"
+#define SHORT_BUILD_VERSION "2.0.420.04.30.23 LA fan"
 
 /**
  * Verbose version identifier which should contain a reference to the location
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "2022-03-13 04:20pm"
+#define STRING_DISTRIBUTION_DATE "2023-04-20 04:20pm"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
